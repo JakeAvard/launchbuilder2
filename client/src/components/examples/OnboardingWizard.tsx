0951b4907a73750bace8f717bd2239c480b51795
JakeAvard <85976457+JakeAvard@users.noreply.github.com>
2026-01-09 10:49:51 -0600

    Delete client/src/components/examples/OnboardingWizard.tsx

--- client/src/components/examples/OnboardingWizard.tsx
+++
@@ -1,5 +0,0 @@
-import { OnboardingWizard } from "../OnboardingWizard";
-
-export default function OnboardingWizardExample() {
-  return <OnboardingWizard onComplete={() => console.log("Onboarding complete")} />;
-}
